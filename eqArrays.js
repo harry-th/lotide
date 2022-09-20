349222777df1cc1e4bd0b356bf85de796d6475bb
harry thomas <harryt1905@gmail.com>
2022-09-20 00:10:36 -0400

    compares arrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = require('./assertEqual.js');
+let eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if ((array1[i] !== array2[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
