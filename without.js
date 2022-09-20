8da77dba3a14ef5852b4c34e199c086c1d122b22
harry thomas <harryt1905@gmail.com>
2022-09-20 10:06:33 -0400

    removes elements specified from an array

---
+++ without.js
@@ -0,0 +1,32 @@
+let assertArraysEqual = require('./assertArraysEqual.js');
+console.log(assertArraysEqual);
+let without = function(array, conditions) {
+  let answer = [];
+  let isItInThere = function(item) {
+    for (let condition of conditions) {
+      if (item === condition) {
+        return false;
+      }
+    }
+    return true;
+  };
+  for (let item of array) {
+    if (isItInThere(item)) {
+      answer.push(item);
+    }
+  }
+  return answer;
+};
+
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without(["1", "2", "3"], ['hello', 2, "3"]));
+console.log(without(["1", "2", "3"], ['1', 2, "3"]));
+
+
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
