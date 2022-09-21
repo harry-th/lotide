c416071491c3b0b969af4e1b06829e3521b32b1b
harry thomas <harryt1905@gmail.com>
2022-09-20 22:03:03 -0400

    add letter counting function

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+let assertEqual = require('./assertEqual');
+
+let countLetters = (word) => {
+  let answer = {};
+  for (let letter of word) {
+    if (!/\w/.test(letter)) continue;
+    answer[letter] ? answer[letter] += 1 : answer[letter] = 1;
+  }
+  return answer;
+};
+
+console.log(countLetters('hello there'));
+
+
+assertEqual(countLetters('lhl').l,2);
+assertEqual(countLetters('lhl').h,1);
+
