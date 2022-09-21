395f793a857edde7abe336f35b06453cceea1512
harry thomas <harryt1905@gmail.com>
2022-09-21 00:58:44 -0400

    removed comment

--- eqObject.js
+++ eqObject.js
@@ -1,13 +1,7 @@
 let assertEqual = require('./assertEqual');
 let eqArrays = require('./eqArrays');
 const eqObjects = function(object1, object2) {
-//   let checkArrayEquality = function(array1, array2) {
-//     if (array1.length !== array2.length) return false;
-//     for (let i = 0; i < array1.length; i++) {
-//       if (array1[i] !== array2[i]) return false;
-//     }
-//     return true;
-//   };
+
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
