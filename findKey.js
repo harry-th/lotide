fa13399e1f9741b7cefe0ad24c1db025cf7be209
harry thomas <harryt1905@gmail.com>
2022-09-22 14:53:36 -0400

    find key by value function added

---
+++ findKey.js
@@ -0,0 +1,27 @@
+let assertEqual = require('./assertEqual');
+
+const findKey = (object, callback) => {
+  for (let item in object) {
+    if (callback(object[item])) return item;
+  }
+  return false;
+};
+
+const generateKey = (argument) => {
+  return findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 'hello' }
+  }, argument);
+};
+
+
+
+
+assertEqual(generateKey(x => x.stars === 2), 'noma');
+assertEqual(generateKey(x => x.stars === 1), 'Blue Hill');
+assertEqual(generateKey(x => x.stars === 3), 'Akaleri');
+assertEqual(generateKey(x => x.stars === 'hello'), 'Akelarre');
