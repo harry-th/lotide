ca6a939c3949f6cdc51ddd7b0f39fa79ba3ad553
harry thomas <harryt1905@gmail.com>
2022-09-26 13:58:49 -0400

    test revision

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,11 +1,11 @@
-const chai = require('chai')
-const assert = chai.assert
+// const chai = require('chai')
+// const assert = chai.assert
 
-const assertEqual = require('../assertEqual')
+// const assertEqual = require('../assertEqual')
 
-describe('assertEqual', () => {
-    let string = 'hello'
-    it('should return equal when given two equal elements, and false when they are not equal', () => {
+// describe('assertEqual', () => {
+//     let string = 'hello'
+//     it('should return equal when given two equal elements, and false when they are not equal', () => {
 
-    })
-})
+//     })
+// })
