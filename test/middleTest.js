8ff73d27f3eaf0565d41f14656d549c2ea35e6b7
harry thomas <harryt1905@gmail.com>
2022-09-26 14:31:27 -0400

    middle test implemented

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,14 @@
-let assertArraysEqual = require('../assertArraysEqual')
-let middle = require('../middle.js')
+const assert = require('chai').assert;
+const middle   = require('../middle');
+describe("#middle", () => {
+  it("returns 3 for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+  it("returns [4, 5] for [1,2,3,4,5,6,7,8]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [4, 5]); 
+  });
+  it("returns [4] for [1,2,3,4,5,6,7]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7]), [4]); 
+  })
+});
 
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]),[4,5]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]),[4]);
