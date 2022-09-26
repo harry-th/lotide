02dbd8bf7ad415f331bed8e1a01b373ad4993920
harry thomas <harryt1905@gmail.com>
2022-09-26 13:31:26 -0400

    eqArrays updated for test file structure

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,10 @@
-
 let eqArrays = function(array1, array2) {
   let length;
   let length1 = array1.length;
   let length2 = array2.length;
-  length1 > length2 ? length = length1 : length = length2;
+  length1 > length2 ? 
+  length = length1 : 
+  length = length2;
   for (let i = 0; i < length; i++) {
     if ((array1[i] !== array2[i])) return false;
   }
@@ -12,10 +13,3 @@ let eqArrays = function(array1, array2) {
 
 
 module.exports = eqArrays;
-
-// {assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
-//   assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
-//   assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
-//   assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
-//   assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);}
-
