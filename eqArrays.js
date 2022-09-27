c2641709f172cb1d2c0435f1fa1f0e52c1667ad1
harry thomas <harryt1905@gmail.com>
2022-09-27 01:21:54 -0400

    added recursive function to eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -2,14 +2,18 @@ let eqArrays = function(array1, array2) {
   let length;
   let length1 = array1.length;
   let length2 = array2.length;
-  length1 > length2 ? 
-  length = length1 : 
-  length = length2;
+  length1 > length2 ? length = length1 : length = length2;
   for (let i = 0; i < length; i++) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i],array2[i])) return false;
+      else continue;
+    } else if (Array.isArray(array1[i]) || Array.isArray(array2[i])) {
+      return false;
+    }
     if ((array1[i] !== array2[i])) return false;
   }
   return true;
 };
-
+console.log(eqArrays([[1,2],2,[[1,2],2]],[[1,2],2,[[1,2],2]]));
 
 module.exports = eqArrays;
